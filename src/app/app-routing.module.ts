dbc7e050efe41d9008b782d5df945ec578b396f6
biswaranjansahoo900 <biswaranjansahoooffical@gmail.com>
2023-02-02 16:45:00 +0530

    Added files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { QrCodeComponent } from './qr-code/qr-code.component';
+import { RegisterComponent } from './register/register.component';
+import { UserTypeComponent } from './user-type/user-type.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: UserTypeComponent },
+  {path:"register", component: RegisterComponent},
+  {path:"contactDetails", component: ContactDetailsComponent},
+  {path:"qrCode", component: QrCodeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+})          
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { UserTypeComponent } from './user-type/user-type.component';
+import { RegisterComponent } from './register/register.component';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { QrCodeComponent } from './qr-code/qr-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserTypeComponent,
+    RegisterComponent,
+    ContactDetailsComponent,
+    SplashScreenComponent,
+    QrCodeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
